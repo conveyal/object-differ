62ad4163e517175666b441c4251f6017e37a699b
Andrew Byrd <andrew@fastmail.net>
2019-09-16 13:27:40 +0800

    remove module-info, declare automatic module name, see issue #3

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.conveyal.kryo_tools {
-    requires kryo;
-    requires com.google.common;
-    requires trove4j;
-    requires slf4j.api;
-    exports com.conveyal.kryo;
-    exports com.conveyal.object_differ;
-}
